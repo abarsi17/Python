12e981c043491e4cb5ff129aec47ea6cf233c307
abarsi17 <72034409+abarsi17@users.noreply.github.com>
2022-12-21 10:27:26 +0100

    Modificación de la estructuración de los ficheros

--- PRUEBAS/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py
-
-# Aqui hay que incluir todos los archivos que estén en este package
-# Esto hay que realizarlo por cada package que tengamos
-""" SE PUEDE INCLUIR DE DIFERENTES MANERAS"""
-__all__ = ['fichero']
-#import .fichero
