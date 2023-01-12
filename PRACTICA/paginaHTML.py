6e31f023de6a59a777458d531961fb64d67099ee
abarsi17 <abarsi@alumni.uv.es>
2023-01-12 12:55:47 +0100

    POO con herencia y creando una pagina web

---
+++ PRACTICA/paginaHTML.py
@@ -0,0 +1,47 @@
+import documentPOO
+
+# paginaHTML hereda de la clase padre Document
+class paginaHTML(documentPOO.Document):
+    def __init__(self, titulo='Pagina', autor = 'root'):
+        super().__init__(titulo, autor)
+        self.contenido = [] # Forzar para que sea una cadena vacía
+
+    def head(self):
+        return """
+    <head>
+        <title> %s </title>
+    </head>
+        """ % (self.titulo)
+    
+    def body(self):
+        return """
+    <body>
+        %s
+    </body>
+        """ % (self.devuelve_contenido())
+
+    def footer(self):
+        return """
+    <footer>
+        %s
+    </footer>
+        """ % (self.fecha_creacion)
+
+    def __str__(self):
+        return """
+<html>
+%s
+%s
+%s
+</html>
+        """ % (self.head(), self.body(), self.footer())
+    
+    def __repr__(self):
+        return """<Documento HTML: %s : %d>""" % (self.autor, id(self))
+
+if __name__ == '__main__':
+    html = paginaHTML()
+    html.titulo = "Pagina HTML"
+    html.contenido.append('<p> HOLA MUNDO! </p>')
+
+    print(html)
