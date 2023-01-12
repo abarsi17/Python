500594712bd50cbf3c00706eb2cc73695448b52a
abarsi17 <abarsi@alumni.uv.es>
2023-01-12 14:08:25 +0100

    Modificación para añadir tablas

--- PRACTICA_HTML/paginaHTML.py
+++ PRACTICA_HTML/paginaHTML.py
@@ -1,7 +1,8 @@
-import PRACTICA.documentPOO
+import documentPOO
+import random
 
 # paginaHTML hereda de la clase padre Document
-class paginaHTML(PRACTICA.documentPOO.Document):
+class paginaHTML(documentPOO.Document):
     def __init__(self, titulo='Pagina', autor = 'root'):
         super().__init__(titulo, autor)
         self.contenido = [] # Forzar para que sea una cadena vacía
@@ -35,6 +36,27 @@ class paginaHTML(PRACTICA.documentPOO.Document):
 %s
 </html>
         """ % (self.head(), self.body(), self.footer())
+
+
+    def add_table(self, encabezado, data):
+        theader = ""
+        for h in encabezado:
+            theader += "<th>%s</th>" % h
+        tbody = ""
+        for l in data:
+            tbody += "<tr>"
+            for r in l:
+                tbody += "<td>%s</td>" % r
+        t = """
+        <table border=1>
+            <thead>
+            <tr>%s</tr>
+            </thead>
+            <tbody>
+            %s
+            </tbody>
+        </table>""" % (theader, tbody)
+        self.contenido.append(t)
     
     def __repr__(self):
         return """<Documento HTML: %s : %d>""" % (self.autor, id(self))
@@ -42,6 +64,15 @@ class paginaHTML(PRACTICA.documentPOO.Document):
 if __name__ == '__main__':
     html = paginaHTML()
     html.titulo = "Pagina HTML"
-    html.contenido.append('<p> HOLA MUNDO! </p>')
+    encabezado = ["Fecha", "PROD", "EVOLUTIVO", "DEV & TEST"]
 
+    dias = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes']
+    resultado = ['OK', 'Error', '&nbsp']
+    
+    Data = []
+    for n in range(0, 5):
+        d = [dias[n], random.choice(resultado), random.choice(resultado), random.choice(resultado)]
+        Data.append(d)
+    html.add_table(encabezado, Data)
     print(html)
+
